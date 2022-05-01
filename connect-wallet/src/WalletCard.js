5824c9a08b207aa8f8a05f39340dfaa8e74c7906
ajinksya <ajinkyakalane777@gmail.com>
2022-05-01 23:51:52 +0530

    with window.ether

---
+++ connect-wallet/src/WalletCard.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react'
+import { ethers } from 'ethers';
+
+const WalletCard = () =>
+{
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [defaultAccount, setDefaultAccount] = useState(null);
+    const [userBalance, setUserBalance] = useState(null);
+    const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+    
+    const connectWalletHandler = () =>
+    {
+        if(window.ethereum)
+        {
+            window.ethereum.request({method: 'eth_requestAccounts'})
+            .then(result =>
+                {
+                    accountChangedHandler(result[0]);
+                })
+        }else
+        {
+            setErrorMessage('Install Metamask')
+        }
+    }
+    const accountChangedHandler = (newAccount) =>
+    {
+        setDefaultAccount(newAccount);
+        getUserBalance(newAccount);
+    }
+    const getUserBalance = (address) =>
+    {
+        window.ethereum.request({method: 'eth_getBalance', params: [address, 'latest']})
+        .then(balance => {
+            setUserBalance(ethers.utils.formatEther(balance));
+        })
+    }
+    return(
+        <div className='walletCard'>
+            <h4>{"Connect to MetaMask using window.ethereum methods"}</h4>
+            <button onClick={connectWalletHandler}>{connButtonText}</button>
+            <div className='accountDisplay'>
+                <h3>Address: {defaultAccount}</h3>
+            </div>
+            <div className='balanceDisplay'>
+                <h3>Balance: {userBalance}</h3>
+            </div>
+            {errorMessage}
+        </div>
+    )
+}
+
+export default WalletCard;
